e4010524b3dc74db9da9342b24f93bcff20fc3a5
sorenhoyer <sorenhoyer84@gmail.com>
2017-10-01 00:57:43 +0200

    Initial commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,32 @@
+import React/*, { Component }*/ from 'react';
+import PropTypes from 'prop-types';
+import Todo from './Todo';
+
+const TodoList = ({todos, onDelete, onClick}) => {
+  let todoItems = todos.map(todo => {
+    return <Todo key={todo.id} todo={todo} onDelete={onDelete} onClick={onClick}/>
+  });
+
+  return <ul>{todoItems}</ul>
+}
+
+// class Todos extends Component {
+//   deleteTodo(id) {
+//     this.props.onDelete(id);
+//   }
+
+//   render() {
+//     let todoItems = this.props.todos.map(todo => {
+//       return <Todo key={todo.id} todo={todo} onDelete={this.deleteTodo.bind(this)}/>
+//     });
+
+//     return <ul>{todoItems}</ul>
+//   }
+// }
+
+TodoList.propTypes = {
+  todos: PropTypes.array,
+  onDelete: PropTypes.func
+}
+
+export default TodoList;
